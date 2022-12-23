aaf0b028dcd7642b124dea8563e68738bb1c4a41
Brandon Chikezie <bchikezie30@gmail.com>
2022-12-23 18:00:20 +0100

    Revalidate API update

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -6,10 +6,12 @@ const secret = process.env.SECRET_TOKEN ?? "SECRET_TOKEN";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== "POST") {
     console.log("Must be a POST request");
-    return res.status(401).json({ message: "Invalid request type" });
+    return res.status(400).json({ message: "Invalid request type" });
   }
 
-  const signature = String(req.headers?.["sanity-webhook-signature"]);
+  const signature = String(req.headers?.["sanity-webhook-signature"]).split(
+    ","
+  )[0];
 
   if (signature !== secret) {
     return res.status(401).json({
