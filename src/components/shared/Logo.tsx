5661159f90a5f77ebbd0591bbc9b9b7a7b736dd3
Brandon Chikezie <bchikezie30@gmail.com>
2022-12-24 14:26:23 +0100

    Fix layout shift

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,10 +1,9 @@
 import { DetailedHTMLProps, FC, HTMLAttributes } from "react";
 import Image from "next/image";
-import useWindowSize from "@src/hooks/useWindowSize";
+import classNames from "classnames";
 
 import whiteLogoImage from "@public/images/wrcked.png";
 import grayLogoImage from "@public/images/wrcked-gray.png";
-import { breakpoints } from "@src/utils";
 
 type LogoProps = {
   size?: "sm" | "lg";
@@ -12,23 +11,16 @@ type LogoProps = {
 } & DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
 const Logo: FC<LogoProps> = ({ size = "sm", color = "white", ...props }) => {
-  const { width } = useWindowSize();
-
   return (
     <div className="Logo" {...props}>
       <Image
         src={color === "white" ? whiteLogoImage : grayLogoImage}
         alt="Wrcked logo"
-        width={
-          size === "sm"
-            ? width >= breakpoints.sm
-              ? 100
-              : 85
-            : width >= breakpoints.lg
-            ? 350
-            : 300
-        }
-        // height={size === "sm" ? 14.99 : 52.275}
+        width={350}
+        className={classNames("h-auto", {
+          "w-[85px] lg:w-[100px]": size === "sm",
+          "w-[300px] lg:w-[350px]": size === "lg",
+        })}
       />
     </div>
   );
