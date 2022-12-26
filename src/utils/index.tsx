63341ddf4a4baddf475fcf7d91b31084a8766568
Brandon Chikezie <bchikezie30@gmail.com>
2022-12-26 04:08:03 +0100

    Enviroment variable update

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -7,7 +7,8 @@ export const shortenString = (string: string, length: number = 8): string => {
 export const getBaseUrl = (): string => {
   if (typeof window === "undefined") return "";
 
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  if (process.env.NEXT_PUBLIC_VERCEL_URL)
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 
   if (process.env.RENDER_INTERNAL_HOSTNAME)
     return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
